c8fc45a9c8666509d9775303018efd69574b433e
MyLeek61 <malik161006@gmail.com>
2026-06-19 09:31:32 +0700

    fix: move storage setup into bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,18 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$storagePath = '/tmp/storage';
+foreach ([
+    $storagePath . '/app/public',
+    $storagePath . '/framework/cache/data',
+    $storagePath . '/framework/sessions',
+    $storagePath . '/framework/views',
+    $storagePath . '/logs',
+] as $dir) {
+    is_dir($dir) || mkdir($dir, 0755, true);
+}
+
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+$app->useStoragePath($storagePath);
+
+return $app;
