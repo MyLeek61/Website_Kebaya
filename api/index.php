4b2438b2360c929033af450a99944b3fec878c1a
MyLeek61 <malik161006@gmail.com>
2026-06-19 08:32:39 +0700

    fix: load order in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,12 +1,11 @@
 <?php
-// Tampilkan semua error
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// Tampilkan semua error ke output agar terlihat di log Vercel
+ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
-// Set path penyimpanan ke folder sementara Vercel
+// Set path ke /tmp agar bisa ditulis
 putenv('VIEW_COMPILED_PATH=/tmp');
 putenv('APP_STORAGE=/tmp');
 
-// Load aplikasi
+// Load aplikasi secara langsung
 require __DIR__ . '/../public/index.php';
