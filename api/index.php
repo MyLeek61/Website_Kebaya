a5ce9af5f5fb473afa2aa3d36870d3bf23a6176f
MyLeek61 <malik161006@gmail.com>
2026-06-19 08:24:03 +0700

    fix: load order in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,13 +1,12 @@
 <?php
-// Tampilkan error jika ada
-ini_set('display_errors', '1');
+// Tampilkan semua error
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Cek apakah PHP bisa melihat database?
-// Ganti dengan kredensial Anda untuk tes sederhana
-$host = getenv('DB_HOST') ? "Host ditemukan" : "Host TIDAK ditemukan";
-echo "Status: Laravel Bootstrap Test. " . $host;
+// Set path penyimpanan ke folder sementara Vercel
+putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_STORAGE=/tmp');
 
-// Jika kode di atas memunculkan "Status: Laravel Bootstrap Test. Host ditemukan" di browser,
-// maka masalahnya ada di konfigurasi Laravel/Database di dalam `public/index.php`.
-exit();
+// Load aplikasi
+require __DIR__ . '/../public/index.php';
