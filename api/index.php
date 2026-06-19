da52ecb875b9a3c200077d7164ec1ae7ded9bedb
MyLeek61 <malik161006@gmail.com>
2026-06-19 08:11:01 +0700

    fix: load order in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
+// Tampilkan error jika ada agar kita tahu masalahnya
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
 
-// 1. Set environment variables terlebih dahulu
+// Set path agar Laravel bisa menulis cache di folder sementara Vercel
 putenv('VIEW_COMPILED_PATH=/tmp');
-putenv('APP_STORAGE=/tmp'); 
+putenv('APP_STORAGE=/tmp');
 
-// 2. Load aplikasi setelah konfigurasi di atas siap
+// Load aplikasi
 require __DIR__ . '/../public/index.php';
