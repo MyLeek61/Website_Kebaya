5f9f1c1190d3bb1ccb17cfadbffe335fa6bec960
MyLeek61 <malik161006@gmail.com>
2026-06-19 08:02:21 +0700

    fix: load order in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,8 @@
 <?php
+
+// 1. Set environment variables terlebih dahulu
 putenv('VIEW_COMPILED_PATH=/tmp');
+putenv('APP_STORAGE=/tmp'); 
+
+// 2. Load aplikasi setelah konfigurasi di atas siap
 require __DIR__ . '/../public/index.php';
-if (getenv('IS_VERCEL')) {
-    putenv('APP_STORAGE=/tmp');
-}
